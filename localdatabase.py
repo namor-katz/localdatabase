e7d5d1552f0a5330ef68b1bab5aae6bdeee68f40
Roman <namor925@gmail.com>
2019-07-16 01:13:56 +0700

    scandir and walk

--- localdatabase.py
+++ localdatabase.py
@@ -12,44 +12,74 @@ from tools import *
 db = SqliteDatabase('localdatabase.db')
 
 
-class InfoFile(Model):
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class InfoFile(BaseModel):
     name = CharField()
     full_path = CharField()
     file_hash = CharField()
     file_size = CharField()
-    file_type = CharField()
-    tag = CharField()
-    read = BooleanField()
-    double = BooleanField()
+    file_type = CharField(default=None, null=True)
+    file_lang = CharField(default=None, null=True)
+    page_count = CharField(default=None, null=True)
+    tag = TextField(null=True)
+    notes = TextField(default=None, null=True)
+    read = BooleanField(default=False)
+    double = BooleanField(default=False)
 
-    class Meta:
-        database = db
+
+class Author(BaseModel):
+    name = CharField(default=None, null=True)
+
+
+class Technology(BaseModel):
+    tecnology = CharField(default=None, null=True)
+    level = IntegerField(default=None, null=True)
+
+
+class Book(BaseModel):
+    
+    fname_id = ForeignKeyField(InfoFile, related_name='book', unique=False)
+    fname = CharField()
+    #author = ForeignKeyField(Author, related_name='author', unique=False)
+
 
 #create db and table
-db.create_tables([InfoFile])
+#db.create_tables([InfoFile, Author, Technology, Book])
 
 def add_values(fname):
+    print(fname)
+    
     new_value = InfoFile(
     name = fname.split('/')[-1],
     full_path = fname,
     file_hash = return_hash_file(fname),
-    tag = 'No tag',
-    file_type = 'No type',
-    read = 'f',
-    double = 'f'
+    file_type = get_file_type(fname),
+    file_lang = check_ru_lang(fname),
+    file_size = get_file_size(fname),
+    tag = None,
+    read = 0,
+    double = 0
     )
     new_value.save()
-    
-    
-camas = InfoFile(
-    name ='camasutra',
-    path ='no path',
-    hash_f = 'no hash',
-    tag = 'notag',
-    type = 'pdf',
-    read = 'f',
-    double = 'f'    
-)
-    
 
-#camas.save()
+
+    new_value2 = Book(
+        fname_id = InfoFile.get(InfoFile.full_path==fname).id,
+        fname = InfoFile.get(InfoFile.full_path==fname).name,
+    )
+    new_value2.save(force_insert=True)
+
+
+if __name__ == '__main__':
+    db.create_tables([InfoFile, Author, Technology, Book])
+    dir_path = sys.argv[1]
+    main_list = []
+    for entry in scantree(dir_path):
+        main_list.append(entry.path)
+
+    for i in main_list:
+        add_values(i)
--- tools.py
+++ tools.py
@@ -9,8 +9,10 @@ import os
 import sys
 from hashlib import md5
 import magic
+import PyPDF2
 
 
+# works files
 def return_hash_file(fname):
     '''get selected file, return string md5sum'''
     hash_md5 = md5()
@@ -20,22 +22,14 @@ def return_hash_file(fname):
     return hash_md5.hexdigest()
 
 
-
-def get_dir_content(full_path):
-    '''принять путь к дире, вернуть ейные полные пути'''
-    path_to_files = []
-        
-    for root, dirs, files in os.walk(full_path):
-        for f in files:
-            file_path = os.path.join(root, f)
-            path_to_files.append(file_path)
-
-        for d in dirs:
-            d = os.path.join(root, d)
-            #get_dir_content(dir_path)
-            print(d)
-
-        return path_to_files
+def scantree(full_path):
+    """Recursively yield DirEntry objects for given directory.
+    so 33135038"""
+    for entry in os.scandir(full_path):
+        if entry.is_dir(follow_symlinks=False):
+            yield from scantree(entry.path)
+        else:
+            yield entry
 
 
 def get_file_size(full_path):
@@ -45,14 +39,52 @@ def get_file_size(full_path):
 
 def get_file_type(full_path):
     ''' принять полный путь, вернуть тип файла.
-    если не получилось вернуть отсутствие значения'''
+    если не получилось - вернуть отсутствие значения
+    хуевенько как то оно определяет. возможно, надо будет поменять'''
     try:
-        file_type = magic.from_file(full_path)
+        file_type = magic.Magic(mime=True).from_file(full_path)
         return file_type
     except:
         return None
 
 
+def check_ru_lang(fname):
+    ''' берем имя файла, проверяем все что выше ascii, если есть, 
+    возвращаем True, название русское, скорее всего и книга руссиш '''
+    check_result = []
+    for i in fname:
+        if ord(i) > 128:
+            check_result.append(1)
+    
+    if 1 in check_result:
+        return 'ru'
+    else:
+        return 'en'
+
+
+def get_file_content(fpath):
+    '''принять путь к пдфине, извлечь метадату, вернуть титл и число страниц'''
+    try:
+        with open(fname, 'rb') as f:
+            pdf = PyPDF2.PdfFileReader(f)
+            info = pdf.getDocumentInfo()
+            number_of_pages = pdf.getNumPages()
+            return info.title, number_of_pages
+    except:
+        return None, None
+
+
 if __name__ == '__main__':
     dir_path = sys.argv[1]
-    a = get_dir_content(dir_path)
+    main_list = []
+    '''
+    for entry in scantree(dir_path):
+        main_list.append(entry.path)
+    '''
+    for root, dirs, files in os.walk(dir_path):
+        for f in files:
+            file_path = os.path.join(root, f)
+            if os.path.exists(file_path):
+                main_list.append(file_path)
+
+    print(len(main_list))
