41369a491f0e1bd3fd4b2df7338da154df7add47
Roman <r.katzendorn@emzior.ru>
2019-08-30 16:35:01 +0300

    check emtpy files, add table from broken files, check enable_extensions

--- localdatabase.py
+++ localdatabase.py
@@ -8,12 +8,17 @@
 
 from peewee import *
 from tools import *
+import logging
 
 
 key_words = ['sql', 'python', 'docker', 'java', 'c', 'c++', 'ruby',
     'sqlite', 'postgresql', 'mongodb', 'machin learning', 'AI', 'django',
     'flask', 'linux', 'aws', 'freebsd', 'unix', 'bash',  'orm',  'golang', 
     'go',  'sqlalchemy']
+
+enable_extension = ['pdf', 'doc', 'djavu', 'txt', 'odt', 'docx']
+archive_extension = ['zip', 'rar', 'tar', '7z']
+exclude_extensions = ['mp4', 'mp3']
 
 db = SqliteDatabase('localdatabase.db')
 
@@ -53,37 +58,54 @@ class Technology(BaseModel):
 class Book(BaseModel):
     ''' книга как абстракция '''
     fname_id = ForeignKeyField(InfoFile, related_name='book', unique=False)
+    #technology_id = 
     fname = CharField()
     #author = ForeignKeyField(Author, related_name='author', unique=False)
 
-
+class BrokenFiles(BaseModel):
+    ''' тут файлы нулевого размера'''
+    name = CharField()
+    full_path = CharField()
+    
 #create db and table
 #db.create_tables([InfoFile, Author, Technology, Book])
 
 def add_values(fname):
-    print(fname)
-
-    new_value = InfoFile(
-    name = fname.split('/')[-1],
-    full_path = fname,
-    file_hash = return_hash_file(fname),
-    file_size = get_file_size(fname),
-    file_type = get_file_type(fname),
-    file_lang = check_ru_lang(fname),
-    page_count = get_file_content(fname)[1],
-    tag = None,
-    read = 0,
-    double = 0
-    )
-    new_value.save()
+    #проверяем, стоит ли вообще файл парсить.
+    next_step = False
+    for i in enable_extension:
+        if fname.endswith(i) is True:
+            next_step = True
 
 
-    new_value2 = Book(
-        fname_id = InfoFile.get(InfoFile.full_path==fname).id,
-        fname = InfoFile.get(InfoFile.full_path==fname).name,
-    )
-    new_value2.save(force_insert=True)
+    if next_step is True:
+        print(fname)
 
+        new_value = InfoFile(
+        name = fname.split('/')[-1],
+        full_path = fname,
+        file_hash = return_hash_file(fname),
+        file_size = get_file_size(fname),
+        file_type = get_file_type(fname),
+        file_lang = check_ru_lang(fname),
+        page_count = get_file_content(fname)[1],
+        tag = None,
+        read = 0,
+        double = 0
+                            )
+        #если размер нулевой, ибо у меня есть битые файлы, не пишем, либо пишем в отдельную таблицу. где только путь
+        if new_value.file_size == 0:
+            print('no added {}'.format(new_value.name))
+        else:
+            new_value.save()
+
+'''
+        new_value2 = Book(
+            fname_id = InfoFile.get(InfoFile.full_path==fname).id,
+            fname = InfoFile.get(InfoFile.full_path==fname).name,
+        )
+        new_value2.save(force_insert=True)
+'''
 
 if __name__ == '__main__':
     db.create_tables([InfoFile, Author, Technology, Book])
