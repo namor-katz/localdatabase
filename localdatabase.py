1dbf04839c1e516c8406f7a294b692d9f2099dee
Roman <namor925@gmail.com>
2019-09-06 03:06:04 +0700

    success create part db usesqlalchemy

--- localdatabase.py
+++ localdatabase.py
@@ -6,30 +6,29 @@
 #  Copyright 2018 roman <roman@roman-pc>
 # нужно: получить полные пути ко всем файлам.
 
-from peewee import *
 from tools import *
 import logging
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///new.db', echo=True)
+Base = declarative_base()
 
 
-key_words = ['sql', 'python', 'docker', 'java', 'c', 'c++', 'ruby',
-    'sqlite', 'postgresql', 'mongodb', 'machin learning', 'AI', 'django',
-    'flask', 'linux', 'aws', 'freebsd', 'unix', 'bash',  'orm',  'golang', 
-    'go',  'sqlalchemy']
 
-enable_extension = ['pdf', 'doc', 'djavu', 'txt', 'odt', 'docx']
+key_words = ['AI', 'algoritms', 'aws', 'bash', 'c', 'c++', 'cryptography',
+ 'django', 'docker', 'english', 'flask', 'freebsd', 'go', 'golang',
+ 'haskell', 'java', 'linux', 'machin learning', 'mongodb', 'mysql',
+ 'oracle', 'orm', 'php', 'postgresql', 'python', 'ruby', 'sql', 'sqlalchemy',
+ 'sqlite', 'svg', 'unix']
+
+enable_extension = ['pdf', 'doc', 'djavu', 'txt', 'odt', 'docx', 'epub']
 archive_extension = ['zip', 'rar', 'tar', '7z']
 exclude_extensions = ['mp4', 'mp3']
 
-db = SqliteDatabase('localdatabase.db')
-
-
-class BaseModel(Model):
-    class Meta:
-        database = db
-
-
+'''
 class InfoFile(BaseModel):
-    ''' тут у нас инфа о файле '''
     name = CharField()
     full_path = CharField()
     file_hash = CharField()
@@ -42,31 +41,50 @@ class InfoFile(BaseModel):
     read = BooleanField(default=False)
     double = BooleanField(default=False)
     assessment = IntegerField(default=None, null=True)
-
-
+'''
+'''
 class Author(BaseModel):
-    '''автор, опционально'''
+    #\'\'\'автор, опционально'\'\'\
     name = CharField(default=None, null=True)
+'''
+
+class Author(Base):
+    '''тот же автор, вид сбоку'''
+    __tablename__ = 'author'
+    id = Column(Integer,  primary_key=True)
+    name = Column(String)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return self.name
 
 
-class Technology(BaseModel):
-    ''' технология, например питон, уровень, например средний'''
+class Technology(Base):
+    __tablename__ = 'technology'
+    id = Column(Integer, primary_key=True)
+    technology = Column(String)
+    level = Column(String)
+
+'''
+class Technology2(BaseModel):
     tecnology = CharField(default=None, null=True)
     level = IntegerField(default=None, null=True)
-
-
+'''
+'''
 class Book(BaseModel):
-    ''' книга как абстракция '''
     fname_id = ForeignKeyField(InfoFile, related_name='book', unique=False)
     #technology_id = 
     fname = CharField()
     #author = ForeignKeyField(Author, related_name='author', unique=False)
+'''
 
+'''
 class BrokenFiles(BaseModel):
-    ''' тут файлы нулевого размера'''
     name = CharField()
     full_path = CharField()
-    
+'''
 #create db and table
 #db.create_tables([InfoFile, Author, Technology, Book])
 
@@ -108,6 +126,10 @@ def add_values(fname):
 '''
 
 if __name__ == '__main__':
+    print('ff')
+    # Создание таблицы
+    Base.metadata.create_all(engine)
+    '''
     db.create_tables([InfoFile, Author, Technology, Book])
     dir_path = sys.argv[1]
     main_list = []
@@ -116,3 +138,4 @@ if __name__ == '__main__':
 
     for i in main_list:
         add_values(i)
+'''
